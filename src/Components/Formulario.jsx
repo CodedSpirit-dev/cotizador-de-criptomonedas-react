ed525986a2a4f8e9a5c6d38db98d267a08f061cc
HalfSeraphic <96750594+HalfSeraphic@users.noreply.github.com>
2022-07-14 17:24:31 -0500

    commit

---
+++ src/Components/Formulario.jsx
@@ -0,0 +1,34 @@
+import styled from "@emotion/styled";
+import React from "react";
+import useSelectMonedas from "../Hooks/useSelectMonedas";
+
+const InputSubmit = styled.input`
+  background-color: #9497ff;
+  border: none;
+  width: 100%;
+  padding: 10px;
+  color: #fff;
+  font-weight: 700;
+  text-transform: uppercase;
+  font-size: 20px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #7a7dfe;
+    cursor: pointer;
+  }
+`;
+
+const Formulario = () => {
+  const [SelectMonedas] = useSelectMonedas();
+
+  return (
+    <form>
+      <SelectMonedas />
+      <InputSubmit type="submit" value="Cotizar" />
+    </form>
+  );
+};
+
+export default Formulario;
---
+++ src/Hooks/useSelectMonedas.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const useSelectMonedas = () => {
+
+    const SelectMonedas = () => {
+        <>
+            <label>Select Monedas</label>
+        </>
+    }
+
+    return[SelectMonedas]
+}
+
+export default useSelectMonedas

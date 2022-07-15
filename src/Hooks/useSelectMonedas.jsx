a22276a8451b4b74b26231c113c3a519d26101ad
HalfSeraphic <VIradVT@gmail.com>
2022-07-15 17:48:11 -0500

    ??

--- src/Hooks/useSelectMonedas.jsx
+++ src/Hooks/useSelectMonedas.jsx
@@ -1,16 +1,16 @@
 import styled from '@emotion/styled'
 import React from 'react'
+
+
 const Label = styled.label`
     color: #fff;
-    `
-    <label htmlFor=""></label>
-
+`
 
 const useSelectMonedas = (label) => {
 
     const SelectMonedas = () => (
         <>
-            <label>Select Monedas</label>
+            <Label>{label}</Label>
         </>
     )
 
